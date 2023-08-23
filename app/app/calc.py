2902d0cd0842ffab69da92c660a797cc62ea40da
Ervin Bosenbacher <admin@tradingpits.com>
2023-08-23 19:18:39 +0100

    added core, unit tests, etc

---
+++ app/app/calc.py
@@ -0,0 +1,23 @@
+"""
+calculator functions
+"""
+
+
+def add(x, y):
+    """
+    add x and y and return the result
+    :param x:
+    :param y:
+    :return:
+    """
+    return x + y
+
+
+def subtract(x, y):
+    """
+    subtract y from x and return the result
+    :param x:
+    :param y:
+    :return:
+    """
+    return x - y
