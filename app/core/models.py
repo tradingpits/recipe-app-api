8cc53376fddf507109fa630fb0ce0c4ec357d34c
Ervin Bosenbacher <admin@tradingpits.com>
2023-08-30 20:49:17 +0100

    added user management interface

--- app/core/models.py
+++ app/core/models.py
@@ -13,12 +13,31 @@ class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
         """Create, save and return a new user"""
+        if not email:
+            raise ValueError("User must have an email address.")
+
         print(f"called create_user with: {email}, {password}")
-        user = self.model(email=email, **extra_fields)
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
 
         return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        """Create, save and return a new superuser"""
+
+        if not email:
+            raise ValueError("Superuser must have an email address.")
+
+
+        print(f"Called create_superuser with: {email}, {password}")
+        super_user = self.model(email=self.normalize_email(email=email), **extra_fields)
+        super_user.is_staff = True
+        super_user.is_superuser = True
+        super_user.set_password(password)
+        super_user.save(using=self._db)
+
+        return super_user
 
 
 class User(AbstractBaseUser, PermissionsMixin):
